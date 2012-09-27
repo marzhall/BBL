4852aac7168efc4c42f5a7a43099a6ebb645f2ec
Marshall Conover <marzhall@isochrono.us>
2012-09-26 23:19:36 -0400

    Reverted to an older working version, removed unn files
    .o's and .hi's are not needed in the official repo.
    
    Added type signatures *again.* 9_9
    
    Reverted to old map-style commandlist; also confirmed tcp server works!
    Will implement parsing later.

--- DatabaseCommands.hs
+++ DatabaseCommands.hs
@@ -5,50 +5,51 @@ import System.Exit
 import System.IO
 import FieldsParser
 
---intersect    :: (Ord k, Show k) => Map String (Map k Field) -> IO ()
+intersect                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
 intersect printFunc readFunc db = do 
-                         putStrLn "What two tables would you like to see the intersection of?"
-                         printFunc "Table One:"
-                         mapOne <- getLine
-                         if (member mapOne db) then do
-                             printFunc "Table Two:"
-                             mapTwo <- getLine
-                             if (member mapTwo db) then
-                                 mapM_ printFunc (keys (intersection (db ! mapOne) (db ! mapTwo)))
-                             else
-                                 printFunc "The second table does not exist; returning you to main."
-                         else
-                             printFunc "The first table does not exist; returning you to main."
+                                  putStrLn "What two tables would you like to see the intersection of?"
+                                  printFunc "Table One:"
+                                  mapOne <- getLine
+                                  if (member mapOne db) then do
+                                      printFunc "Table Two:"
+                                      mapTwo <- getLine
+                                      if (member mapTwo db) then
+                                          mapM_ printFunc (keys (intersection (db ! mapOne) (db ! mapTwo)))
+                                      else
+                                          printFunc "The second table does not exist; returning you to main."
+                                  else
+                                      printFunc "The first table does not exist; returning you to main."
 
---printFields    :: Show k => Map String (Map k Field) -> IO () 
+printFields                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
 printFields printFunc readFunc db = do
-                           putStrLn "Which table would you like to see the fields of?"
-                           answer <- getLine
-                           if (member answer db) then 
-                              mapM_ printFunc $ keys (db ! answer)
-                           else
-                              printFunc "Not a table."
+                                    putStrLn "Which table would you like to see the fields of?"
+                                    answer <- getLine
+                                    if (member answer db) then 
+                                       mapM_ printFunc $ keys (db ! answer)
+                                    else
+                                       printFunc "Not a table."
 
+printFieldInfo                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
 printFieldInfo printFunc readFunc db = do
-                              printFunc "Which table would you like to see a field's info from?"
-                              answer <- getLine
-                              if (member answer db) then do
-                                 printFunc "Which field would you like to see the info of?"
-                                 fieldToPrint <- getLine
-                                 if (member fieldToPrint (db ! answer)) then 
-                                    printFunc $ show ((db ! answer) ! fieldToPrint)
-                                 else
-                                    printFunc ("Not a field of table " ++ answer)
-                              else
-                                 printFunc "Not a table."
+                                       printFunc "Which table would you like to see a field's info from?"
+                                       answer <- getLine
+                                       if (member answer db) then do
+                                          printFunc "Which field would you like to see the info of?"
+                                          fieldToPrint <- getLine
+                                          if (member fieldToPrint (db ! answer)) then 
+                                             printFunc $ show ((db ! answer) ! fieldToPrint)
+                                          else
+                                             printFunc ("Not a field of table " ++ answer)
+                                       else
+                                          printFunc "Not a table."
               
---findField    :: Show k => Map String (Map k Field) -> IO () 
+findField                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
 findField printFunc readFunc db = do
-                         putStrLn "What field would you like to search for?"
-                         answer <- getLine
-                         mapM_ printFunc $ keys $ Data.Map.filter (\f -> member answer f) db
+                                  putStrLn "What field would you like to search for?"
+                                  answer <- getLine
+                                  mapM_ printFunc $ keys $ Data.Map.filter (\f -> member answer f) db
 
---commandList :: Map [Char] (Map String (Map String Field) -> IO ())
+commandList                    :: ([Char] -> IO ()) -> [Char] -> Map [Char] (Map String (Map [Char] Field) -> IO ())
 commandList printFunc readFunc = fromList [  ("exit", (\_ -> System.Exit.exitWith ExitSuccess))
                                            , ("findfield", findField printFunc readFunc)
                                            , ("halp", (\_ -> mapM_ printFunc $ keys (commandList printFunc readFunc))) --eat the db in order to match the other functions
--- haskellParsingSmall.hs
+++ haskellParsingSmall.hs
@@ -7,21 +7,11 @@ import Text.Parsec
 import FieldsParser
 import DatabaseCommands
 import System.Environment
---import TcpServer
-
--- for interacting with the db in ghci
---getDB = do
-        --let filename = "progressDB.txt"
-        --handle <- openFile filename ReadMode
-        --contents <- hGetContents handle
-        --case parse tables filename contents of
-            --Left err       -> empty
-            --Right database -> database 
+import TcpServer
 
 main =  do
         let filename = "progressDB.txt"
-        let localCommands = (commandList print getLine)
-
+        --let localCommands = (commandList print getLine)
         args <- getArgs
         let runServer = (/=) [] args             --if there's an argument, we're running the server
         handle <- openFile filename ReadMode
@@ -30,19 +20,19 @@ main =  do
            Left err -> do
                        print err
                        System.Exit.exitWith $ ExitFailure 1
-           Right database -> if runServer then do
-                                 putStrLn "What would you like to do? (For help, type \"Halp.\")"
-                                 answer <- getLine
-                                 case member answer localCommands of
-                                     True -> (localCommands ! answer) database
-                                     False -> do 
-                                              print "That's not an option. Here are your commands: "
-                                              (localCommands ! "halp") database
-                             else forever $ do
-                                 putStrLn "What would you like to do? (For help, type \"Halp.\")"
-                                 answer <- getLine
-                                 case member answer localCommands of
-                                     True -> (localCommands ! answer) database
-                                     False -> do 
-                                              print "That's not an option. Here are your commands: "
-                                              (localCommands ! "halp") database
+           Right database -> do start database
+                                 --putStrLn "What would you like to do? (For help, type \"Halp.\")"
+                                 --answer <- getLine
+                                 --case member answer localCommands of
+                                     --True -> (localCommands ! answer) database
+                                     --False -> do 
+                                              --print "That's not an option. Here are your commands: "
+                                              --(localCommands ! "halp") database
+                             --else forever $ do
+                                 --putStrLn "What would you like to do? (For help, type \"Halp.\")"
+                                 --answer <- getLine
+                                 --case member answer localCommands of
+                                     --True -> (localCommands ! answer) database
+                                     --False -> do 
+                                              --print "That's not an option. Here are your commands: "
+                                              --(localCommands ! "halp") database
