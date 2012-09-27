a938a260a3615a06a914f1dcbbec889fd2204477
Marshall Conover <marzhall@isochrono.us>
2012-09-26 23:21:28 -0400

    Quick type fix 9_6
    Should have seen this one coming.

--- DatabaseCommands.hs
+++ DatabaseCommands.hs
@@ -5,7 +5,7 @@ import System.Exit
 import System.IO
 import FieldsParser
 
-intersect                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
+intersect                       :: ([Char] -> IO ()) -> IO String -> Map String (Map [Char] Field) -> IO ()
 intersect printFunc readFunc db = do 
                                   putStrLn "What two tables would you like to see the intersection of?"
                                   printFunc "Table One:"
@@ -20,7 +20,7 @@ intersect printFunc readFunc db = do
                                   else
                                       printFunc "The first table does not exist; returning you to main."
 
-printFields                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
+printFields                       :: ([Char] -> IO ()) -> IO String -> Map String (Map [Char] Field) -> IO ()
 printFields printFunc readFunc db = do
                                     putStrLn "Which table would you like to see the fields of?"
                                     answer <- getLine
@@ -29,7 +29,7 @@ printFields printFunc readFunc db = do
                                     else
                                        printFunc "Not a table."
 
-printFieldInfo                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
+printFieldInfo                       :: ([Char] -> IO ()) -> IO String -> Map String (Map [Char] Field) -> IO ()
 printFieldInfo printFunc readFunc db = do
                                        printFunc "Which table would you like to see a field's info from?"
                                        answer <- getLine
@@ -43,13 +43,13 @@ printFieldInfo printFunc readFunc db = do
                                        else
                                           printFunc "Not a table."
               
-findField                       :: ([Char] -> IO ()) -> [Char] -> Map String (Map [Char] Field) -> IO ()
+findField                       :: ([Char] -> IO ()) -> IO String -> Map String (Map [Char] Field) -> IO ()
 findField printFunc readFunc db = do
                                   putStrLn "What field would you like to search for?"
                                   answer <- getLine
                                   mapM_ printFunc $ keys $ Data.Map.filter (\f -> member answer f) db
 
-commandList                    :: ([Char] -> IO ()) -> [Char] -> Map [Char] (Map String (Map [Char] Field) -> IO ())
+commandList                    :: ([Char] -> IO ()) -> IO String -> Map [Char] (Map String (Map [Char] Field) -> IO ())
 commandList printFunc readFunc = fromList [  ("exit", (\_ -> System.Exit.exitWith ExitSuccess))
                                            , ("findfield", findField printFunc readFunc)
                                            , ("halp", (\_ -> mapM_ printFunc $ keys (commandList printFunc readFunc))) --eat the db in order to match the other functions
