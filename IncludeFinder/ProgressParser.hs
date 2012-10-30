b9ec2bcd3866565385c65899035b1b2b48a36219
Marshall Conover <marzhall@isochrono.us>
2012-10-29 21:19:08 -0400

    Corrected things like {1} being caught as includes

--- IncludeFinder/ProgressParser.hs
+++ IncludeFinder/ProgressParser.hs
@@ -7,29 +7,29 @@ import Text.Parsec.Language
 
 include :: Parser String
 include = do
-              char '{'
-              fileName <- many1 $ alphaNum <|> noneOf "\\/?*\"><|&} "
-              many $ noneOf "}"
-              char '}'
-              return fileName
+    char '{'
+    fileName <- many1 $ alphaNum <|> noneOf "\\/?*\"><|&} "
+    many $ noneOf "}"
+    char '}'
+    return fileName
 
 junk :: Parser String
 junk = do
-       many1 $ noneOf "{"
-       return ""
+    many1 $ noneOf "{"
+    return ""
 
 preprocessor :: Parser String
 preprocessor = do
-                  char '{'
-                  char '&'
-                  instructions <- many $ noneOf "}"
-                  char '}'
-                  return ""
+    char '{'
+    oneOf "&1234567890"
+    instructions <- many $ noneOf "}"
+    char '}'
+    return ""
 
 includes :: Parser [String]
 includes  = do
-            bracedCode <- many $ try preprocessor <|> include <|> junk
-            eof
-            return $ populated bracedCode
-            where 
-                populated list = filter (\x -> x /= "") list
+    bracedCode <- many $ try preprocessor <|> include <|> junk
+    eof
+    return $ populated bracedCode
+    where 
+        populated list = filter (\x -> x /= "") list
