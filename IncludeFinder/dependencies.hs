ddd24e810c0376a8781c00fec2cc8a7ebd01e9e5
Marshall Conover <marzhall@isochrono.us>
2012-12-04 21:34:13 -0500

    Added the ability to create a DB of dependencies for each .i file
    Runing dependies.exe creates a db of all of the files in a directory
    passed to it, as well as the files that depend on that any specific file
    to compile. On running, after the DB is built, the user can type the
    name of a file in the directory passed, and a file
    <filename.dependencies" will print. That "dependencies" file will hold
    all of the files that need its parent to compile.

---
+++ IncludeFinder/dependencies.hs
@@ -0,0 +1,58 @@
+import ProgressParser
+import Data.Map hiding (foldl, filter)
+import Data.List
+import System.Directory
+import Text.Parsec
+import Text.ParserCombinators.Parsec.Error
+import Text.ParserCombinators.Parsec.Pos
+import System.Environment
+import Control.Monad
+import System.IO
+
+parseFile          :: String -> IO (Either ParseError [String])
+parseFile fileName = catch (readFile fileName >>= (\contents -> return $ parse includes contents contents))
+                           (\_ -> return $ Left $ newErrorMessage (UnExpect ": file cannot be read or found.") (newPos fileName 0 0)) 
+
+prettyPrinter          :: [String] -> String
+prettyPrinter fileList = foldl (\aggregator file -> (file ++ "\n") ++ aggregator) "" populatedFileList
+    where populatedFileList = (filter ((/=) "") fileList)  
+
+dependencyFold                          :: Map String [String] -> String -> String -> IO (Map String [String])
+dependencyFold dependMap child fileName = do
+    parsedFile <- parseFile fileName
+    case parsedFile of
+        Left err -> return $ insertWith (++) fileName ((show err):(child:(dependMap ! child))) dependMap
+        Right results -> if results /= [] 
+                            then do
+                                parents <- mapM (dependencyFold newDB fileName) results
+                                return $ unionsWith (++) parents 
+                            else
+                                return $ newDB
+            where newDB = insertWith (++) fileName (child:(dependMap ! child)) dependMap
+
+main = do
+    args <- getArgs 
+    dependencyDB <- do
+        catch (readFile "dependencies.cache" >>= (\contents -> return $ fromList $ read contents))
+              (\_ -> do
+                  let askUser = (>=) 1 (length args)
+                  directory <- if askUser 
+                      then do
+                          print "What's the directory, boss?" 
+                          getLine
+                      else return (args !! 1)
+
+                  fileList <- getDirectoryContents directory
+                  dependencyFold <- mapM (dependencyFold (fromList [("", [])]) "") fileList
+                  let uniquedMap = Data.Map.map nub $ unionsWith (++) dependencyFold
+                  writeFile "dependencies.cache" (show $ toList $ Data.Map.map nub $ unionsWith (++) dependencyFold)
+                  return uniquedMap)
+
+    forever $ do
+        print "What's the file you need, chief?" 
+        filename <- getLine
+        let result = if (member filename dependencyDB) then
+                         (dependencyDB ! filename)
+                     else
+                         ["Couldn't find this file in the directory we read."]
+        writeFile (filename ++ ".dependencies") ((prettyPrinter result) ++ "\n")
--- IncludeFinder/includeTree.hs
+++ IncludeFinder/includeTree.hs
@@ -14,7 +14,7 @@ addSpaces     :: String -> Int -> String
 addSpaces spaces 0 = spaces
 addSpaces spaces indent = addSpaces (' ':spaces) (indent - 1)
 
---childTrees                              :: Int -> IncludeTree [Either ParseError String] -> [Char]
+childTrees                              :: Int -> IncludeTree (Either ParseError String) -> [Char]
 childTrees indent (NoIncludes filename) = "\n" ++ addSpaces "" indent ++ stripEndlines (show filename)
 childTrees indent (IncludeFile filename includes) = "\n" ++ addSpaces "" indent
     ++ (stripEndlines $ show filename)
@@ -23,20 +23,3 @@ childTrees indent (IncludeFile filename includes) = "\n" ++ addSpaces "" indent
 printTree (NoIncludes filename) = (show filename)
 printTree (IncludeFile filename includes) = stripEndlines (show filename)
     ++ concatMap (childTrees 4) includes
-
-
---makeMap :: IncludeTree (Either ParseError String) -> Map String [String]
---makeMap (NoIncludes filename) = case filename of
-    --Right filename -> fromList [(filename, [])]
-    --Left filename -> fromList [(show filename, [])]
---makeMap (IncludeFile filename includes) = case filename of
-    --Right filename -> fromListWith (++) [(filename, concatMap makeMapHelper includes)]
-    --Left filename -> fromListWith (++) [(show filename, [])]
---
---makeMapHelper :: IncludeTree (Either ParseError String) -> [(String, [String])]
---makeMapHelper (NoIncludes filename) childFile = case filename of
-    --Right filename -> [(filename, [childFile])]
-    --Left filename -> [(show filename, [childFile])]
---makeMapHelper (IncludeFile filename includes) childFile = case filename of
-    --Right filename -> [(childFile, concatMap makeMapHelper includes filename)]
-    --Left filename -> [(show filename, childFile)]
