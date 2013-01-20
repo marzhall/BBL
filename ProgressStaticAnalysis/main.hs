09ae1da15058eb14a1c790ca4da7c05ee356379a
Marshall Conover <marzhall@isochrono.us>
2013-01-20 15:33:08 -0500

    moving the stsatic anaylsis project to its own repository.

--- ProgressStaticAnalysis/main.hs
+++
@@ -1,6 +0,0 @@
-import LanguageDefinition 
-import Text.Parsec
-import Text.Parsec.Token
-import Text.Parsec.Language
-
-main = show $ parse openedge "test" "FOR FIRST lol:\n MESSAGE 'herp'.\n END."
